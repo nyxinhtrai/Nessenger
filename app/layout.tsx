9ceda6fa0b15c9b11501d0d80b8364d59f554abe
Ny Nguyen <npsony2@gmail.com>
2025-03-11 13:12:38 +0700

    import toaster in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
+
 import { Geist, Geist_Mono } from "next/font/google";
+
+import ToasterContext from "./context/ToasterContext";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,6 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ToasterContext />
         {children}
       </body>
     </html>
