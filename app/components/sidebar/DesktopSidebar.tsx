653339cdc1dd6d0c3129d4998ddcef8080b2de31
Ny Nguyen <npsony2@gmail.com>
2025-03-12 14:05:39 +0700

    get current user to show avt

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -1,10 +1,20 @@
 'use client';
 
-import useRoutes from '@/app/hooks/useRoutes'
 import { useState } from 'react';
-import DesktopItem from './DesktopItem';
+import { User } from '@prisma/client';
 
-const DesktopSidebar = () => {
+import useRoutes from '@/app/hooks/useRoutes'
+
+import DesktopItem from './DesktopItem';
+import Avatar from '../Avatar';
+
+interface DesktopSidebarProps {
+    currentUser: User
+}
+
+const DesktopSidebar:React.FC<DesktopSidebarProps> = ({
+    currentUser
+}) => {
     const routes = useRoutes();
     const [isOpen, setIsOpen] = useState(false);
 
@@ -55,6 +65,26 @@ const DesktopSidebar = () => {
                         />
                     ))}
                 </ul>
+            </nav>
+            <nav
+                className='
+                    mt-4
+                    flex
+                    flex-col
+                    justify-between
+                    items-center
+                '
+            >
+                <div
+                    onClick={() => setIsOpen(true)}
+                    className='
+                        cursor-pointer
+                        hover:opacity-75
+                        transition
+                    '
+                >
+                    <Avatar user={currentUser} />
+                </div>
             </nav>
         </div>
     );
