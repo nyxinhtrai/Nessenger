9fbf6bf31ec289895ece0bdd72c23a0e8605602d
Ny Nguyen <npsony2@gmail.com>
2025-04-14 11:29:18 +0700

    add icon for web

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Nessenger",
   description: "Nessenger",
+  icons: {
+    icon: '/images/logo.png'
+  }
 };
 
 export default function RootLayout({
